072e3110b8822308cd5bebe9fcba752c2b0d4c6d
Obrigado <Obrigado@DESKTOP-J7KLEJN>
2017-11-18 23:40:10 -0200

    meu primeiro commit dentro do netbeans

---
+++ src/exemplo/git/netbeans/ExemploGitNetbeans.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplo.git.netbeans;
+
+/**
+ *
+ * @author Obrigado
+ */
+public class ExemploGitNetbeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Exemplo de um prjeto qual quer com git e netbeans");// TODO code application logic here
+    }
+    
+}
